9a10c95e6611c9e2a69186000db27520f8e93f00
Jorge Pastor <hyorch@gmail.com>
2025-04-11 13:27:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_user" "usu01"{
-    name = "usu01"
+  name = "usu01"
 }
 
 resource "aws_iam_access_key" "ak-1"{
-    user = aws_iam_user.usu01.name
+  user = aws_iam_user.usu01.name
 }
 
 resource "aws_iam_user" "usu02"{
-    name = "usu02"
-    tags ={"manual":"true"}
+  name = "usu02"
+  tags ={"manual":"true"}
 }
 
 resource "aws_iam_access_key" "ak-2"{
-    user = aws_iam_user.usu02.name
+  user = aws_iam_user.usu02.name
 }
 
 resource "aws_iam_access_key" "ak-22"{
-    user = aws_iam_user.usu02.name
+  user = aws_iam_user.usu02.name
 }
