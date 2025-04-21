4dbea9adf7ee64ce72d34ca6f858f2de8af4aaa2
hyorch <hyorch@gmail.com>
2025-04-21 12:24:16 +0200

    tf fmt

--- providers.tf
+++ providers.tf
@@ -8,15 +8,15 @@ terraform {
   required_version = ">= 1.0.0"
 
   backend "s3" {
-    #profile        = "HyorchAdmin"
-    bucket         = "hyorch-terraform-github-actions"
-    key            = "terraform.tfstate"
-    region         = "eu-west-1"    
+    profile = "HyorchAdmin"
+    bucket  = "hyorch-terraform-github-actions"
+    key     = "terraform.tfstate"
+    region  = "eu-west-1"
   }
- 
+
 }
 
 provider "aws" {
-  #profile = "HyorchAdmin"
-  region = "eu-south-2"
+  profile = "HyorchAdmin"
+  region  = "eu-south-2"
 }
