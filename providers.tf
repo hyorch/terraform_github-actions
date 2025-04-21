ba9a00fa6eaff06a08bedceec16cb95af7650a40
hyorch <hyorch@gmail.com>
2025-04-21 11:15:35 +0200

    s3 backend

--- providers.tf
+++ providers.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    #profile        = "HyorchAdmin"
+    bucket         = "hyorch-terraform-github-actions"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"    
+  }
+ 
+}
+
 provider "aws" {
   #profile = "HyorchAdmin"
   region = "eu-south-2"
